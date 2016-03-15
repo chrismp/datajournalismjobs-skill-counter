9ecdda425e5888529f1dbcee9b5756bbb0309652
unknown <cpersaud@DEV-CPERSAUD-US.cortera.local>
2016-03-15 16:21:49 -0400

    write wordcounts to TXT

--- skills-wordcounter.py
+++ skills-wordcounter.py
@@ -6,12 +6,28 @@ filename=	"DataJournalismJobs.com submissions - Form Responses 1.csv"
 filePath=	directory+filename
 
 skillsString=	''
-with open(filePath,"rb") as csvFile:
+with open(filePath, "rt", encoding="utf8") as csvFile:
 	csvReader=	csv.reader(csvFile)
 	for row in csvReader:
 		skillsString += row[4]
 		skillsString += ' '
 
-print skillsString
-print "============"
-print re.findall(r"\b[A-Za-z]+\b", skillsString)
+	skillsString=	skillsString.lower().replace('.','')
+
+
+wordList=	re.findall(r"\b[A-Za-z]+\b", skillsString)
+# wordFreq=	[]
+wordFreq=	{}
+for w in wordList:
+	# wordFreq.append(wordList.count(w))
+	wordFreq[w]=	wordList.count(w)
+	# print(w,wordList.count(w))
+
+# pairs=	zip(wordList,wordFreq)
+# for i in list(pairs):
+# 	print(i)
+
+outputFile=	open("DataJournalismJobsDotComSkillsWordcounts.txt",'w')
+outputFile.write("Word\tCount\n")
+for word in wordFreq:
+	outputFile.write("{}\t{}\n".format(word,wordFreq[word]))
