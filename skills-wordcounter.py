457b673ae03f6bd20b944ccbd3f6be4366046987
Chris P <>
2016-03-14 22:16:58 -0400

    added primitive word extractor

--- skills-wordcounter.py
+++ skills-wordcounter.py
@@ -1,10 +1,17 @@
 import csv
+import re
 
 directory=	"raw/"
 filename=	"DataJournalismJobs.com submissions - Form Responses 1.csv"
 filePath=	directory+filename
 
+skillsString=	''
 with open(filePath,"rb") as csvFile:
 	csvReader=	csv.reader(csvFile)
 	for row in csvReader:
-		print row[4]
+		skillsString += row[4]
+		skillsString += ' '
+
+print skillsString
+print "============"
+print re.findall(r"\b[A-Za-z]+\b", skillsString)
